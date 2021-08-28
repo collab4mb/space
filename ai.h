f78bd440a265aeea5cd424690df75a95e9ad5d4f
Captain4LK <captain4lk@tutanota.com>
2021-08-28 17:58:15 +0200

    Add functions to AI for clarification

--- ai.h
+++ ai.h
@@ -36,15 +36,18 @@ void _ai_idle(void *param0) {
 static void _ai_move(void *param0) {
   Ent *ent = (Ent *)param0;
 
+  //If target has been destroyed, revert to being idle
   if(ent->ai.target==NULL||ent->ai.target->generation!=ent->ai.target_gen) {
     _ai_set_state(ent,_ai_entinfo[ent->ai.type].state_idle);
   }
 
+  //Rotate towards target
   ent->angle = lerp_rad(ent->angle,rot_vec2(vec2_swap(sub2(ent->ai.target->pos,ent->pos))),0.04f);
+
+  //Accelerate towards target, until close enough, then attack
   ent->vel = add2(ent->vel,mul2_f(vec2_swap(vec2_rot(ent->angle)),0.025f));
   if (magmag2(ent->vel)>MAX_SPEED2)
     ent->vel = mul2_f(norm2(ent->vel),MAX_SPEED);
-
   if(magmag2(sub2(ent->pos,ent->ai.target->pos))<=m_square(10.f))
     _ai_set_state(ent,_ai_entinfo[ent->ai.type].state_attack);
 }
@@ -52,6 +55,7 @@ static void _ai_move(void *param0) {
 static void _ai_attack(void *param0) {
   Ent *ent = (Ent *)param0;
 
+  //If target has been destroyed, revert to being idle
   if(ent->ai.target==NULL||ent->ai.target->generation!=ent->ai.target_gen)
     _ai_set_state(ent,_ai_entinfo[ent->ai.type].state_idle);
 
@@ -62,6 +66,8 @@ static void _ai_attack(void *param0) {
   ent->vel = mul2_f(ent->vel, 0.9f);
 
   //Shoot
+  //TODO: implement too attack states (TYPE_ATTACK0,TYPE_ATTACK1). One has a duration of 0, the other a duration of 'shot delay' (TODO: implement delta time).
+  //In ATTACK0 the actuall attack takes place, in ATTACK1 the entity 'idles'.
   if(input_key_pressed(SAPP_KEYCODE_A)) {
     Vec2 e_dir = vec2_swap(vec2_rot(ent->angle));
     Ent *e = add_ent((Ent) {
@@ -109,4 +115,5 @@ static void ai_run(void *param0) {
 }
 //-------------------------------------
 
+#undef MAX_SPEED
 #undef MAX_SPEED2
