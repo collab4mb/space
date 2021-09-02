be71ade257b4708351a4a9f939000a4e0c67ce2c
ishidex2 <artsbyduo@gmail.com>
2021-09-02 23:20:43 +0300

    Deduplication

--- build.h
+++ build.h
@@ -2,7 +2,8 @@
 
 typedef enum {
   _build_Option_Pillar,
-  _build_Option_Wall
+  _build_Option_Wall,
+  _build_Option_COUNT
 } _build_Option;
 
 typedef struct {
@@ -80,6 +81,41 @@ int _build_interp(int from, int to, float mul) {
   return (int)_build_easeinout((float)from, (float)to, fmaxf(fminf(self.appear_anim*mul, 1.0f), 0.0f));
 }
 
+bool _build_make_ent(Ent *ent, Shader *shader) {
+  if (self.option_selected == _build_Option_Pillar) {
+    *ent = (Ent) {
+      .art = Art_Pillar,
+      .pos = add2(state->player->pos, mul2_f(vec2_swap(vec2_rot(state->player->angle)), 10.0)),
+      .height = 0,
+      .x_rot = 0,
+      .transparency = 1.0,
+      .passive_rotate_axis = vec3_y,
+    };
+    if (shader != NULL) *shader = Shader_Standard;
+    return true;
+  }
+  else if (self.option_selected == _build_Option_Wall) {
+    if (_build_connection_finalized(&self.connection)) {
+      Vec2 diff = sub2(self.connection.end->pos, self.connection.begin->pos);
+      Vec2 pos = add2(self.connection.begin->pos, div2_f(diff, 2.0f));
+
+      *ent = (Ent) {
+        .art = Art_Plane,
+        .pos = pos,
+        .angle = atan2f(diff.x, diff.y)+PI_f/2.0f,
+        .scale = { mag2(diff)/2.0f+0.4f, 4.0f, 1.0f },
+        .height = -1.0f,
+        .collider.size = mag2(diff)/2.0f,
+        .collider.shape = Shape_Line,
+        .collider.weight = 1000.0f,
+      };
+      if (shader != NULL) *shader = Shader_ForceField;
+      return true;
+    }
+  }
+  return false;
+}
+
 static Ent* add_ent(Ent ent);
 bool build_event(const sapp_event *ev) {
   switch (ev->type) {
@@ -89,35 +125,11 @@ bool build_event(const sapp_event *ev) {
         return true;
       }
       else if (self.appear_anim > 0.0f && ev->key_code == SAPP_KEYCODE_SPACE) {
-        if (self.option_selected == _build_Option_Pillar) {
-          add_ent((Ent) {
-            .art = Art_Pillar,
-            .pos = add2(state->player->pos, mul2_f(vec2_swap(vec2_rot(state->player->angle)), 10.0)),
-            .height = 0,
-            .x_rot = 0,
-            .transparency = 1.0,
-            .passive_rotate_axis = vec3_y,
-          });
+        Ent dest;
+        if (_build_make_ent(&dest, NULL)) {
+          add_ent(dest);
         }
-        else if (self.option_selected == _build_Option_Wall) {
-          if (_build_connection_finalized(&self.connection)) {
-            Vec2 diff = sub2(self.connection.end->pos, self.connection.begin->pos);
-            Vec2 pos = add2(self.connection.begin->pos, div2_f(diff, 2.0f));
-
-            add_ent((Ent) {
-              .art = Art_Plane,
-              .pos = pos,
-              .angle = atan2f(diff.x, diff.y)+PI_f/2.0f,
-              .scale = { mag2(diff)/2.0f+0.4f, 4.0f, 1.0f },
-              .height = -1.0f,
-              .collider.size = mag2(diff)/2.0f,
-              .collider.shape = Shape_Line,
-              .collider.weight = 1000.0f,
-            });
-          }
-          _build_connection_select(&self.connection);
-        }
-      
+        _build_connection_select(&self.connection);
         return true;
       }
     } break;
@@ -128,33 +140,12 @@ bool build_event(const sapp_event *ev) {
 
 static void draw_ent(Mat4 vp, Ent *ent);
 void build_draw_3d(Mat4 vp) {
-  if (self.appearing || self.appear_anim > 0) {
-    if (self.option_selected == _build_Option_Pillar) {
-      sg_apply_pipeline(state->pip[Shader_Standard]);
-      draw_ent(vp, &(Ent) {
-        .art = Art_Pillar,
-        .pos = add2(state->player->pos, mul2_f(vec2_swap(vec2_rot(state->player->angle)), 10.0)),
-        .height = 0,
-        .x_rot = 0,
-        .transparency = lerp(0, 0.5, fmax(fmin(self.appear_anim, 1.0), 0.0)),
-        .passive_rotate_axis = vec3_y,
-      });
-    }
-    else if (self.option_selected == _build_Option_Wall && _build_connection_finalized(&self.connection)) {
-      Vec2 diff = sub2(self.connection.end->pos, self.connection.begin->pos);
-      Vec2 pos = add2(self.connection.begin->pos, div2_f(diff, 2.0f));
-
-
-      sg_apply_pipeline(state->pip[Shader_ForceField]);
-      draw_ent(vp, &(Ent) {
-        .art = Art_Plane,
-        .pos = pos,
-        .transparency = lerp(0, 0.5, fmax(fmin(self.appear_anim, 1.0), 0.0)),
-        .angle = atan2f(diff.x, diff.y)+PI_f/2.0f,
-        .scale = { mag2(diff)/2.0f+0.4f, 4.0f, 1.0f },
-        .height = -1.0f,
-      });
-    }
+  Ent dest;
+  Shader shader;
+  if (_build_make_ent(&dest, &shader) && self.appearing) {
+    sg_apply_pipeline(state->pip[shader]);
+    dest.transparency = 0.5;
+    draw_ent(vp, &dest);
   }
 }
 
@@ -175,7 +166,7 @@ void build_update() {
       }
     }
   }
-  if (!_build_state.appearing) return;
+  if (!_build_state.appearing || _build_state.option_selected != _build_Option_Wall) return;
   _build_connection_set_next(&self.connection, pillar);
   if (self.connection.begin)
     self.connection.begin->bloom = 0.7f;
@@ -192,11 +183,12 @@ void build_draw() {
         ui_frame(300, ui_rel_y(1.0), 1);
         ui_margin(10);
         ui_row(300, ui_rel_y(1.0));
-          for (size_t i = 0; i < (sizeof(_build_options)/sizeof(build_Option)); i += 1) {
+          for (size_t i = 0; i < _build_Option_COUNT; i += 1) {
             ui_margin(5);  
             ui_column(ui_rel_x(1/3.0f), ui_rel_x(1/3.0f)+32); 
               ui_screen(ui_rel_x(1.0f), ui_rel_x(1.0f));
                 if (ui_frame(ui_rel_x(1.0f), ui_rel_x(1.0f), i == self.option_selected ? 2 : 0) && input_mouse_down(0)) {
+                  self.connection = (_build_Connection) { 0 };
                   self.option_selected = i;
                 }
                 ui_screen_anchor_xy(0.5, 0.5);
